c1f5569a054c4622f5de42766bd028a3d4762a63
rcellas <rocio.cejudo.soronellas@gmail.com>
2023-07-13 10:41:43 +0200

    preparation environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsListComponent } from './cards/components/cards-list/cards-list.component';
+import { CardsDetailComponent } from './cards/components/cards-detail/cards-detail.component';
+import { HomeComponent } from './cards/views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsListComponent,
+    CardsDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cards/components/cards-detail/cards-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-detail',
+  templateUrl: './cards-detail.component.html',
+  styleUrls: ['./cards-detail.component.scss']
+})
+export class CardsDetailComponent {
+
+}
